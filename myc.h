e3d9a6a3bb4498af0d165f623355ac8facd06e71
XunRong Tan <tanxrong@gmail.com>
2013-09-09 15:37:49 +0800

    udp

--- myc.h
+++ myc.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <sys/un.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <time.h>
--- time_client.c
+++ time_client.c
@@ -1,54 +1,68 @@
 
+
 #include "myc.h"
+
+static int rcount = 0;
+static int sockfd = 0;
+void dealAlarm(int signum)
+{
+    printf("read from server count %d\n",rcount);
+    close(sockfd);
+    exit(0);
+}
 
 int main(int argc,char* argv[])
 {
-    int sockfd,n;
-    char recvline[MAXLINE + 1];
-    struct sockaddr_in servaddr;
 
-    if (argc != 3)
-    {
-        err_quit("Usage: daytime_client <IPAdress> <message>");
-    }
-
-    if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
-    {
-        err_quit("create socket err");
-    }
+    char recvline[MAXLINE];
+    struct sockaddr_un servaddr;
+    char *sockfile = "/tmp/testsock";
     memset(recvline,0,sizeof(recvline));
-    servaddr.sin_family=AF_INET;
-    servaddr.sin_port=htons(3789);
+    memset(&servaddr,0,sizeof(servaddr));
 
-    if (inet_pton(AF_INET,argv[1],&servaddr.sin_addr) <= 0)
-    {
-        err_sys("IPv4 host addr false");
-    }
+    servaddr.sun_family=AF_UNIX;
+    strncpy(servaddr.sun_path,sockfile,sizeof(servaddr.sun_path));
 
-    if ( connect(sockfd,(struct sockaddr *)&servaddr,sizeof(struct sockaddr_in)) < 0)
-    {
+    if ((sockfd = socket(AF_UNIX,SOCK_STREAM,0)) < 0)
+        err_quit("create socket err");
+    if ( connect(sockfd,(struct sockaddr *)&servaddr,sizeof(struct sockaddr_un)) < 0)
         err_sys("connect error");
-    }
+    setfdnonblock(sockfd);
+    char *msg="client msg";
+    fd_set rfds,wfds;
+    FD_ZERO(&rfds);FD_ZERO(&wfds);
+    FD_SET(sockfd,&rfds);FD_SET(sockfd,&wfds);
+    struct timeval timeout;
+    timeout.tv_sec = 0;timeout.tv_usec = 200;
 
-    if ( (n = write(sockfd,argv[2],strlen(argv[2])+1)) < 0 )
+    alarm(100);
+    signal(SIGALRM,(void*)dealAlarm);
+    while (1)
     {
-        err_sys("send msg fail");
-    }
+        FD_ZERO(&rfds);FD_ZERO(&wfds);
+        FD_SET(sockfd,&rfds);FD_SET(sockfd,&wfds);
+        timeout.tv_sec = 0;timeout.tv_usec = 200;
 
-    while ( (n = read(sockfd,recvline,MAXLINE)) > 0)
-    {
-        recvline[n]='\0';
-        if (fputs(recvline,stdout) == EOF)
+        int ready=0;
+        if ((ready = select(sockfd+1,&rfds,&wfds,NULL,&timeout)) < 0)
+            err_sys("select");
+        else if(ready == 0)
+             continue;
+        else
         {
-            err_sys("fputs error");
+            if(FD_ISSET(sockfd,&wfds) > 0)
+            {
+                write(sockfd,msg,strlen(msg));
+            }
+            if(FD_ISSET(sockfd,&rfds) > 0)
+            {
+                memset(recvline,0,MAXLINE+1);
+                read(sockfd,recvline,MAXLINE);
+                rcount++;
+            }
         }
-    }
 
-    if (n < 0)
-    {
-        err_sys("read error");
     }
 
     return 0;
 }
-
