625ad02b103a398e201e324f1b529586867aee2b
XunRong Tan <tanxrong@gmail.com>
2013-09-17 11:18:33 +0800

    uds epoll

--- uds.c
+++ uds.c
@@ -1,46 +1,23 @@
 
 #include "myc.h"
 
-int main(int argc,char*argv[])
-{
+struct e_conn {
+    int epollfd;
     int sockfd;
-    struct sockaddr_un addr;
-    char buf[MAXLINE];
-    int n;
-    memset(buf,0,MAXLINE);
-    memset(&addr,0,sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    char *sockname = "/tmp/testsock";
-    strncpy(addr.sun_path,sockname,sizeof(addr.sun_path)-1);
-    if((sockfd = socket(AF_UNIX,SOCK_STREAM,0)) < 0)
-        err_sys("create sock");
-    if(bind(sockfd,(struct sockaddr *)&addr,sizeof(addr)) < 0)
-        err_sys("bind");
-    if(listen(sockfd,LISTEN_QUEUE) < 0)
-        err_sys("listen");
+    int used;
+    char rbuf[MAXLINE];
+    char wbuf[MAXLINE];
+};
 
-    int connfd;
-    if ((connfd = accept(sockfd,NULL,0)) < 0 )
-        err_sys("accept");
-    int count = 0;
-    while(1)
-        {
-        if ((n=read(connfd,buf,MAXLINE)) < 0)
-        {
-            perror("read");
-            break;
-        }
-        else if(n == 0)
-            sleep(1);
-        else
-        {
-            count++;
-//            fputs(buf,stdout);
-            memset(buf,0,MAXLINE);
-        }
-    }
-    printf("count %d \n",count);
-    close(sockfd);
-    unlink(sockname);
-    return 0;
+struct e_conn global_conn[CONN_MAX];
+#define EPOLL_FDNUM 6
+int epollfd[EPOLL_FDNUM];
+#define WORKERS_PER_EPOLL 8
+#define WORKER_NUM (EPOLL_FDNUM * WORKERS_PER_EPOLL)
+int worker[WORKER_NUM];
+
+int main(int argc,char *argv[])
+{
+
+
 }
