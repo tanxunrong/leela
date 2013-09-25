b16087298256bd6906d4ae72d54bfa1d0774e46d
tanxunrong <tanxr@tanxunrongtekiiMac.local>
2013-09-25 22:50:05 +0800

    mac os reinstall,fix problems

--- myc.h
+++ myc.h
@@ -16,7 +16,7 @@
 #include <pthread.h>
 
 #define MAXLINE 1000
-#define LISTEN_QUEUE 5
+#define LISTEN_QUEUE 10
 
 void err_quit(char* str)
 {
@@ -37,16 +37,16 @@ void setfdnonblock(int fd)
     fcntl(fd,F_SETFL,fctl);
 }
 
-void set_keepalive(int sockfd,int alive,int idle,int intval,int count)
-{
-    if (alive <= 0 || idle <= 0 || count <= 0 || intval <= 0)
-        err_quit("set keepalive,negative param");
-    if (setsockopt(sockfd,IPPROTO_TCP,SO_KEEPALIVE,(void *)&alive,sizeof(alive)) < 0)
-        err_sys("keep alive");
-    /*if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPIDLE,(void *)&idle,sizeof(idle)) < 0)
-        err_sys("keep idle");
-    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPINTVL,(void *)&intval,sizeof(intval)) < 0)
-        err_sys("keep intval");
-    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPCNT,(void *)&count,sizeof(count)) < 0)
-        err_sys("keep cnt");*/
-}
+//void set_keepalive(int sockfd,int alive,int idle,int intval,int count)
+//{
+//    if (alive <= 0 || idle <= 0 || count <= 0 || intval <= 0)
+//        err_quit("set keepalive,negative param");
+//    if (setsockopt(sockfd,IPPROTO_TCP,SO_KEEPALIVE,(void *)&alive,sizeof(alive)) < 0)
+//        err_sys("keep alive");
+//    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPIDLE,(void *)&idle,sizeof(idle)) < 0)
+//        err_sys("keep idle");
+//    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPINTVL,(void *)&intval,sizeof(intval)) < 0)
+//        err_sys("keep intval");
+//    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPCNT,(void *)&count,sizeof(count)) < 0)
+//        err_sys("keep cnt");
+//}
--- uds.c
+++ uds.c
@@ -1,23 +1,23 @@
 
 #include "myc.h"
 
-struct e_conn {
-    int epollfd;
-    int sockfd;
-    int used;
-    char rbuf[MAXLINE];
-    char wbuf[MAXLINE];
-};
+//struct e_conn {
+//    int epollfd;
+//    int sockfd;
+//    int used;
+//    char rbuf[MAXLINE];
+//    char wbuf[MAXLINE];
+//};
 
-struct e_conn global_conn[CONN_MAX];
-#define EPOLL_FDNUM 6
-int epollfd[EPOLL_FDNUM];
-#define WORKERS_PER_EPOLL 8
-#define WORKER_NUM (EPOLL_FDNUM * WORKERS_PER_EPOLL)
-int worker[WORKER_NUM];
+//struct e_conn global_conn[CONN_MAX];
+//#define EPOLL_FDNUM 6
+//int epollfd[EPOLL_FDNUM];
+//#define WORKERS_PER_EPOLL 8
+//#define WORKER_NUM (EPOLL_FDNUM * WORKERS_PER_EPOLL)
+//int worker[WORKER_NUM];
 
 int main(int argc,char *argv[])
 {
-
+    return 0;
 
 }
