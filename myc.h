7a348e271e13ed31ec4ce075abb85a83aacbf0c2
tanxunrong <tanxrong@gmail.com>
2014-08-14 01:08:22 +0800

    mutli thread test msg queue

--- myc.h
+++ myc.h
@@ -14,39 +14,3 @@
 #include <fcntl.h>
 #include <sys/select.h>
 #include <pthread.h>
-
-#define MAXLINE 1000
-#define LISTEN_QUEUE 10
-
-void err_quit(char* str)
-{
-    fprintf(stderr,"%s",str);
-    exit(EXIT_FAILURE);
-}
-
-void err_sys(char *str)
-{
-    perror(str);
-    exit(errno);
-}
-
-void setfdnonblock(int fd)
-{
-    int fctl = fcntl(fd,F_GETFL);
-    fctl |= O_NONBLOCK;
-    fcntl(fd,F_SETFL,fctl);
-}
-
-//void set_keepalive(int sockfd,int alive,int idle,int intval,int count)
-//{
-//    if (alive <= 0 || idle <= 0 || count <= 0 || intval <= 0)
-//        err_quit("set keepalive,negative param");
-//    if (setsockopt(sockfd,IPPROTO_TCP,SO_KEEPALIVE,(void *)&alive,sizeof(alive)) < 0)
-//        err_sys("keep alive");
-//    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPIDLE,(void *)&idle,sizeof(idle)) < 0)
-//        err_sys("keep idle");
-//    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPINTVL,(void *)&intval,sizeof(intval)) < 0)
-//        err_sys("keep intval");
-//    if (setsockopt(sockfd,IPPROTO_TCP,TCP_KEEPCNT,(void *)&count,sizeof(count)) < 0)
-//        err_sys("keep cnt");
-//}
--- uds.c
+++ uds.c
@@ -1,34 +1,85 @@
 
 #include "myc.h"
+#include "msg_queue.h"
 #include <glib.h>
 gboolean timeout_cb(gpointer data);
+struct lmsg *
+dummy_msg()
+{
+    struct lmsg *ret = g_malloc0(sizeof(*ret));
+//    GTimeZone *zone = g_time_zone_new_local();
+    GDateTime *now = g_date_time_new_now_local();
+    ret->data = g_date_time_format(now,"%F %H:%M:%S");
+    ret->sz = strlen(ret->data) + 1;
+
+    return ret;
+}
 
 static gpointer
-_timer(gpointer *param) {
-    GMainLoop *timeLoop = g_main_loop_new(NULL,false);
+_timer(gpointer param) {
+    GMainLoop *timeLoop = g_main_loop_new(NULL,FALSE);
     g_timeout_add(5,timeout_cb,NULL);
     g_main_loop_run(timeLoop);
-    g_main_loop_unref(timeLoop);
+    g_main_loop_quit(timeLoop);
     return NULL;
 }
 
 
 gboolean timeout_cb(gpointer data)
 {
-    static int i=0;
-    i++;
-    g_print("timeout callback %d times called",i);
-    if(10==i)
+    if (leela_globalmq_pop() != NULL)
     {
-        g_main_loop_quit((GMainLoop*)data);
+        g_debug("global queue not empty");
         return FALSE;
     }
     return TRUE;
 }
 
+gboolean worker_cb_1(gpointer data)
+{
+    struct lmsg_queue *mq = data;
+    struct lmsg *dummy = dummy_msg();
+    if (leela_mq_push(mq,dummy) != 0)
+    {
+        return FALSE;
+    }
+    g_debug("cb_1 called");
+    return TRUE;
+}
+
+gboolean worker_cb_2(gpointer data)
+{
+    struct lmsg_queue *mq = data;
+    if (leela_mq_length(mq) > 10)
+    {
+        leela_globalmq_push(mq);
+        return FALSE;
+    }
+    g_debug("cb_2 called");
+    return TRUE;
+}
+
+static gpointer
+_worker(gpointer param) {
+    GMainLoop *timeLoop = g_main_loop_new(NULL,FALSE);
+    struct lmsg_queue *mq = leela_mq_create(1);
+    int t1,t2;
+    t1 = g_random_int() % 100;
+    t2 = g_random_int() % 100;
+    g_timeout_add(t1,worker_cb_1,mq);
+    g_timeout_add(t2,worker_cb_2,mq);
+    g_main_loop_run(timeLoop);
+    g_main_loop_quit(timeLoop);
+    return NULL;
+}
+
+
 int main(int argc,char *argv[])
 {
+    leela_mq_init();
     GThread *timerThread = g_thread_new("timer",_timer,NULL);
+    GThread *workerThread = g_thread_new("worker",_worker,NULL);
     g_thread_join(timerThread);
+    g_thread_join(workerThread);
     return 0;
 }
